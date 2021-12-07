c64151f05da3c81106997a947fd03410088a6d6f
Jikun Liu <jikun@umich.edu>
2021-12-07 11:46:46 -0500

    new data added

--- Data Processing/datacleaning.py
+++ Data Processing/datacleaning.py
@@ -2,17 +2,32 @@ import pandas as pd
 import os
 
 os.chdir('../data')
-df = pd.read_csv('DSNY_Monthly_Tonnage_Data.csv')
-df = df[['BOROUGH','MONTH','REFUSETONSCOLLECTED','PAPERTONSCOLLECTED','MGPTONSCOLLECTED']]
-df['MONTH'] = pd.to_datetime(df['MONTH'])
-df['year'] = df['MONTH'].dt.year
-df['month'] = df['MONTH'].dt.month
-df.rename(columns={'BOROUGH':'Borough','REFUSETONSCOLLECTED':'Refuse',
-                   'PAPERTONSCOLLECTED':'Paper','MGPTONSCOLLECTED':'MGP'},
-                   inplace = True)
-df = df[['Borough','year','month','Refuse','Paper','MGP']]
-df = df.loc[df['year']>=2020]
+# df = pd.read_csv('1RAW-DSNY_Monthly_Tonnage_Data.csv')
+# df = df[['BOROUGH','MONTH','REFUSETONSCOLLECTED','PAPERTONSCOLLECTED','MGPTONSCOLLECTED']]
+# df['MONTH'] = pd.to_datetime(df['MONTH'])
+# df['year'] = df['MONTH'].dt.year
+# df['month'] = df['MONTH'].dt.month
+# df.rename(columns={'BOROUGH':'Borough','REFUSETONSCOLLECTED':'Refuse',
+#                    'PAPERTONSCOLLECTED':'Paper','MGPTONSCOLLECTED':'MGP'},
+#                    inplace = True)
+# df = df[['Borough','year','month','Refuse','Paper','MGP']]
+# df = df.loc[df['year']>=2020]
+# df.dropna(inplace=True)
+# df.replace('Staten Island', 'Staten',inplace=True)
+# path = os.getcwd()+'\\tonnage.csv'
+# df.to_csv(path,index=False)
+
+df = pd.read_csv('1RAW-New_York_State_Statewide_COVID-19_Testing.csv')
+df = df[['Test Date','County','New Positives','Cumulative Number of Positives',
+         'Total Number of Tests Performed',
+         'Cumulative Number of Tests Performed']]
+df = df.loc[df['County'].isin(['Kings','Queens','Bronx','New York','Richmond'])]
+df['Test Date'] = pd.to_datetime(df['Test Date'])
+df['year'] = df['Test Date'].dt.year
+df['month'] = df['Test Date'].dt.month
+df.rename(columns={'County':'Borough'}, inplace = True)
 df.dropna(inplace=True)
-df.replace('Staten Island', 'Staten',inplace=True)
-path = os.getcwd()+'\\tonnage.csv'
+df.replace(['Kings','Queens','Bronx','New York','Richmond'],
+           ['Brooklyn','Queens','Bronx','Manhattan','Staten'],inplace=True)
+path = os.getcwd()+'\\covid-nyc.csv'
 df.to_csv(path,index=False)
--- Data Processing/dataloading.py
+++ Data Processing/dataloading.py
@@ -1,24 +1,46 @@
 import pandas as pd
 import os
 
-# only include file specified in list
-def dataloader(file_list):
+# only include file specified in list; merge at request
+def dataloader(file_list, merge=False):
     os.chdir('../data')
     out_df = pd.DataFrame({'Borough': []})
+    df_dict = {}
 
     for name in file_list:
         if name == "scorecard":
-            if len(out_df.index) == 0:
-                out_df = pd.read_csv('scorecard-inspection.CSV')
+            if merge == False:
+                df_dict[name] = pd.read_csv('scorecard-inspection.CSV')
             else:
-                df = pd.read_csv('scorecard-inspection.CSV')
-                out_df = out_df.merge(df, on='Borough')
+                if len(out_df.index) == 0:
+                    out_df = pd.read_csv('scorecard-inspection.CSV')
+                else:
+                    df = pd.read_csv('scorecard-inspection.CSV')
+                    out_df = out_df.merge(df, on='Borough')
+
         elif name == "tonnage":
-            if len(out_df.index) == 0:
-                out_df = pd.read_csv('tonnage.csv')
+            if merge == False:
+                df_dict[name] = pd.read_csv('tonnage.csv')
             else:
-                df = pd.read_csv('tonnage.csv')
-                out_df = out_df.merge(df, on='Borough')
+                if len(out_df.index) == 0:
+                    out_df = pd.read_csv('tonnage.csv')
+                else:
+                    df = pd.read_csv('tonnage.csv')
+                    out_df = out_df.merge(df, on='Borough')
+
+        elif name == "covid":
+            if merge == False:
+                df_dict[name] = pd.read_csv('covid-nyc.csv')
+            else:
+                if len(out_df.index) == 0:
+                    out_df = pd.read_csv('covid-nyc.csv')
+                else:
+                    df = pd.read_csv('covid-nyc.csv')
+                    out_df = out_df.merge(df, on='Borough')
+
+    if merge == False:
+        return df_dict
+
     return out_df
 
-print(dataloader(["scorecard",'tonnage']))
+print(dataloader(["scorecard",'tonnage','covid']))
